6f2a5ffa72c6aa23d9f6ff468a145a5ed563684d
sz741858 <45990695+sz741858@users.noreply.github.com>
2019-12-18 14:17:38 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+// file: script.js
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyC6UxwzG4WskR_AAE2GWUoWDLjIbs8G4ME",
+    authDomain: "project-1095469750188232048.firebaseapp.com",
+    databaseURL: "https://project-1095469750188232048.firebaseio.com",
+    projectId: "project-1095469750188232048",
+    storageBucket: "project-1095469750188232048.appspot.com",
+    messagingSenderId: "783214213079"
+};
+firebase.initializeApp(config);
+
+//create firebase database reference
+var dbRef = firebase.database();
+var contactsRef = dbRef.ref('contacts');
+
+//load older conatcts as well as any newly added one...
+contactsRef.on("child_added", function(snap) {
+  console.log("added", snap.key, snap.val());
+  $('#contacts').append(contactHtmlFromObject(snap.val()));
+});
+
+//save contact
+$('.addValue').on("click", function( event ) {  
+    event.preventDefault();
+    if( $('#name').val() != '' || $('#email').val() != '' ){
+      contactsRef.push({
+        name: $('#name').val().replace(/<[^>]*>/ig, ""),
+        email: $('#email').val().replace(/<[^>]*>/ig, ""),
+        location: {
+          city: $('#city').val().replace(/<[^>]*>/ig, ""),
+          state: $('#state').val().replace(/<[^>]*>/ig, ""),
+          zip: $('#zip').val().replace(/<[^>]*>/ig, "")
+        }
+      })
+      contactForm.reset();
+    } else {
+      alert('Please fill atlease name or email!');
+    }
+  });
+
+//prepare conatct object's HTML
+function contactHtmlFromObject(contact){
+  console.log( contact );
+  var html = '';
+  html += '<li class="list-group-item contact">';
+    html += '<div>';
+      html += '<p class="lead">'+contact.name+'</p>';
+      html += '<p>'+contact.email+'</p>';
+      html += '<p><small title="'+contact.location.zip+'">'+contact.location.city+', '+contact.location.state+'</small></p>';
+    html += '</div>';
+  html += '</li>';
+  return html;
+}
